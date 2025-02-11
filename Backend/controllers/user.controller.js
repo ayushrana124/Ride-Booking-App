3629d7bfd8ce10e17cca283d4b03377d1516f7c0
Nirbhay Sisodia <nirbhaysis888@gmail.com>
2025-02-12 02:49:02 +0530

    login user route done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -23,3 +23,24 @@ module.exports.registerUser = async (req,res,next) => {
   const token = user.generateAuthToken();
   res.status(201).json({ token , user });
 }
+
+module.exports.loginUser = async (req,res,next) => {
+    const errors = validationResult(req); // agr routes m jo checks kiye {body} m, kuch galat hua to req m miljayega
+    if(!errors.isEmpty()){ //if there is error
+     return res.status(400).json({errors  : errors.array()}); 
+    }
+    
+    const {email, password} = req.body;
+    const user = await userModel.findOne({email}).select('+password'); //means while finding user, include password field also bcs we add select false in userSchema
+    if(!user){
+        return res.status(401).json({error : 'Invalid email or password'});
+    }
+    
+    const isMatch = await user.comparePassword(password);
+    if(!isMatch){
+        return res.status(401).json({error : 'Invalid email or password'});
+    }
+    
+    const token = user.generateAuthToken();
+    res.status(200).json({token, user});
+}
